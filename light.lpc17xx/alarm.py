15ade434d4fdfb353e204e2a1bb638f094f6846b
William Breidenthal <willhb@gmail.com>
2014-02-11 21:37:22 -0800

    Comments are good.

--- light.lpc17xx/alarm.py
+++ light.lpc17xx/alarm.py
@@ -10,12 +10,12 @@ alarmMin = 1
 alarmHour = 7
 
 while 1:
-	time.sleep(5)
+	time.sleep(5) #don't do stuff for a while
 	x = time.localtime()
-	print x
+	print x #print to debug
 	if x[3] == alarmHour:
 		if x[4] == alarmMin:
-			ser.write("o\n")
+			ser.write("o\n")#send this string and the microcontroller handles the rest
 		
 
 
